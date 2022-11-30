695e4e0ddcfc87b46da2f22f74203521639f5e87
parkjm <parkjm@intechsvc.com>
2022-11-30 16:48:43 +0900

    Integrated Service

--- src/App.js
+++ src/App.js
@@ -1,7 +1,125 @@
-import logo from './logo.svg';
+import React from 'react';
 import Todo from './Todo';
+import AppTodo from './AppTodo';
 import './App.css';
+import { List, Paper } from '@mui/material';
+import { Container } from '@mui/system';
+import { call } from './service/ApiService';
 
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      items: []
+    }
+  }
+
+  add = (item) => {
+    call("/todo","POST",item).then((response) =>
+    this.setState({ items: response.data })
+    );
+
+    /*
+    const thisItems = this.state.items;
+    item.id = "ID-"+thisItems.length;
+    item.done = false;
+    thisItems.push(item);
+    this.setState({ items: thisItems });
+    console.log("items : ", this.state.items);
+    */
+  }
+
+  delete = (item) => {
+    call("/todo","DELETE",item).then((response) => 
+      this.setState({ items: response.data })
+    )
+
+    /*
+    const thisItems = this.state.items;
+    console.log("Before Update Items : ", this.state.items);
+    const newItems = thisItems.filter(e => {
+      let flag = e.id !== item.id;
+      console.log('e : ',e);
+      console.log('item : ',item);
+      console.log(flag);
+      return flag;
+    });
+    console.log(newItems);
+    this.setState({ items: newItems }, () => {
+      console.log("Update Items : ", this.state.items)
+    })
+    */
+  }
+
+  update = (item) => {
+    call("/todo", "PUT", item).then((response) => 
+      this.setState({ items: response.data })
+    )
+  }
+
+  componentDidMount(){
+
+    call("/todo", "GET", null).then((response) =>
+      {console.log(response);
+      return this.setState({ items: response.data});
+      }
+    );
+    
+    /*
+    const requestOptions = {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    };
+
+    fetch("http://localhost:8080/todo", requestOptions)
+      .then((response) => response.json())
+      .then(
+        (response) => {
+          this.setState({
+            items: response.data,
+          });
+        },
+        (error) => {
+          this.setState({
+            error,
+          })
+        }
+      );
+    */
+  }
+
+  render(){
+    var todoItems = this.state.items.length > 0 && (
+      <Paper style={{ margin: 16 }}>
+        <List>
+          {this.state.items.map((item, idx) => (
+            <Todo 
+              item={item} 
+              key={item.id} 
+              delete={this.delete} 
+              update={this.update}
+              />
+          ))}
+        </List>
+      </Paper>
+    );
+
+    return <div className='App'>
+              <Container maxWidth="md">
+                <AppTodo add={this.add} />
+                <div className='TodoList'>{todoItems}</div>
+              </Container>
+          </div>;
+    
+    /*
+    var todoItems = this.state.items.map((item, idx) => (
+        <Todo item={item} key={item.id} />
+      ));
+    */
+  }
+}
+
+/*
 function App() {
   return (
     <div className="App">
@@ -23,5 +141,5 @@ function App() {
     </div>
   );
 }
-
+*/
 export default App;
---
+++ src/AppTodo.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { TextField, Paper, Button, Grid} from "@mui/material";
+
+class AppTodo extends React.Component {
+    constructor(props){
+        super(props);
+        console.log(props);                         // 전달된 인자들
+        this.state = { item: { title: "" } };       // 사용자의 입력을 저장할 오브젝트
+        this.add = props.add;
+    }
+
+    onInputChange = (e) => {
+        const thisItem = this.state.item;
+        thisItem.title = e.target.value;
+        this.setState({ item: thisItem });
+        console.log(this.state.item === thisItem);   // 동일함
+        console.log(this.state.item);               // item 내부
+        console.log(this.state.item.title);         // title의 value
+    }
+
+    onButtonClick = () => {
+        this.add(this.state.item);
+        this.setState({ item: { title: "" } });     
+        console.log(this);                          // AppTodo
+        console.log(this.add);                      // add 함수
+        console.log(this.state);                    // item 객체명까지
+        console.log(this.state.item);               // item 객체내부
+    }
+
+    enterKeyEventHandler = (e) => {
+        if (e.key === 'Enter'){
+            this.onButtonClick();
+        }
+    }
+
+    render(){
+        return (
+            <Paper style={{ margin: 16, padding: 16 }}>
+                <Grid container>
+                    <Grid xs={11} md={11} item style={{ paddingRighjt: 16 }}>
+                        <TextField 
+                            placeholder="Add Todo here" 
+                            fullWidth
+                            onChange={this.onInputChange}
+                            onKeyPress={this.enterKeyEventHandler}
+                            value={this.state.item.title} 
+                        />
+                    </Grid>
+                    <Grid xs={1} md={1} item>
+                        <Button 
+                            fullWidth color="secondary" 
+                            variant="outlined"
+                            onClick={this.onButtonClick}
+                            >
+                            +
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Paper>
+        
+        );
+    }
+
+}
+
+export default AppTodo;
