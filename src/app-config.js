577efa0c5f759b7b4b144d88c8074e694cb60ffd
namu6747 <namu6747@naver.com>
2022-12-14 23:24:37 +0900

    complete

--- src/app-config.js
+++ src/app-config.js
@@ -4,6 +4,8 @@ const hostname = window && window.location && window.location.hostname;
 
 if (hostname === "localhost"){
     backendHost = "http://localhost:8080";
+} else {
+    backendHost = "http://prodtodobackend-env.eba-qv2dvcmi.ap-northeast-1.elasticbeanstalk.com";
 }
 
 export const API_BASE_URL = `${backendHost}`;
